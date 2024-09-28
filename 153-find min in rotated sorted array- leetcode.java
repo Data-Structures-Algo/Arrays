4c43e90452e8234c960f6f51a26a422fe2c05c2d
Gourav Kumar <137706422+Gouravkumar08@users.noreply.github.com>
2024-09-28 14:26:30 +0530

    Create 153-find min in rotated sorted array- leetcode.java

---
+++ 153-find min in rotated sorted array- leetcode.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMin(int[] arr) {
+        int left=0;
+        int right=arr.length-1;
+
+        while(left<right){
+            int mid=(left+right)/2;
+            if(arr[mid]<arr[right]){
+                right=mid;
+            }else{
+                left=mid+1;
+            }
+        }
+        return arr[left];
+    }
+}
